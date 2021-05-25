31da08c9a6afd194123f97cca9442bb71dc73bc6
Ralf S. Engelschall <rse@engelschall.com>
2021-05-25 20:03:24 +0200

    fix name

--- smp/Gruntfile.js
+++ smp/Gruntfile.js
@@ -21,7 +21,7 @@ module.exports = function (grunt) {
                         } ]
                     ],
                     browserifyOptions: {
-                        standalone: "ClientBrowser",
+                        standalone: "Sample",
                         debug: false
                     }
                 }
